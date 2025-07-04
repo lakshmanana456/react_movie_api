d677b8bde282d19274bab0ed2ceee98a990153f2
lakshmanana456 <elakshmanan10501@gmail.com>
2025-07-04 14:19:21 +0530

    3rd movie deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom'
 import WatchList from "./pages/WatchList"
 import Home from './pages/Home'
 import Navbar from './pages/Navbar'
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
     <WatchListProvider>
-      <BrowserRouter>
+      <HashRouter>
       <Navbar/>
         <Routes>
           <Route path='/' element={<Home/>}/>
@@ -20,7 +20,7 @@ function App() {
           
 
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
       </WatchListProvider>
     </>
   )
